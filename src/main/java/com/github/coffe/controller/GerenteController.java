c5d84f0c04914b3f1d7e3930ba397e48d8b5b7dd
Jhully Ferreira <jhully.ferreira@cs.up.edu.br>
2025-05-28 00:47:58 -0300

    Inicio da implementação do menu principal

---
+++ src/main/java/com/github/coffe/controller/GerenteController.java
@@ -0,0 +1,4 @@
+package com.github.coffe.controller;
+
+public class GerenteController {
+}
---
+++ src/main/java/com/github/coffe/view/MainView.java
@@ -0,0 +1,48 @@
+package com.github.coffe.view;
+
+import java.util.Scanner;
+
+public class MainView {
+    Scanner sc = new Scanner(System.in);
+
+    public void inicializar(){
+        int op = 1;
+        do{
+            System.out.println("Insira uma opção para continuar: ");
+            System.out.println("[1] - Cliente");
+            System.out.println("[2] - Funcionário");
+            System.out.println("[0] - Sair");
+            op = sc.nextInt();
+            switch (op){
+                case 1:
+                    System.out.println("[1] - Login");
+                    System.out.println("[2] - Cadastrar");
+                    if (sc.nextInt() == 1){
+                        loginCliente();
+                    } else if (sc.nextInt() == 2){
+                        autoCadastro();
+                    } else {
+                        System.out.println("Entrada inválida");
+                        return;
+                    }
+                    break;
+
+                case 2: loginFuncionario(); break;
+                default:
+                    System.out.println("Entrada inválida! Tente novamente");
+            }
+        } while(op != 0);
+    }
+    //login e autocadastro -> cliente view -Pedro
+    public void loginCliente(){
+
+    }
+
+    public void autoCadastro(){
+
+    }
+    //funcionario view - Jhully
+    public void loginFuncionario(){
+
+    }
+}
